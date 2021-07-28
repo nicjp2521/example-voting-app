acf3e110755c79a17d25adc0c0eb52f5554933d1
nicjp2521 <88043712+nicjp2521@users.noreply.github.com>
2021-07-28 13:46:46 -0400

    Update server.js

--- result/server.js
+++ result/server.js
@@ -30,7 +30,7 @@ var pool = new pg.Pool({
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
-    pool.connect('postgres://postgres_user:postgres_passoword@db/postgres', function(err, client, done) {
+    pool.connect(function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
